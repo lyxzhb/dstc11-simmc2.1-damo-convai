a7ddd9c1406827a6811c2104f5af6fa59195964b
lyxzhb <longyuxing123321@gmail.com>
2022-10-21 12:03:22 +0800

    add code for task4

---
+++ task4/run_scripts/simmc2.1/evaluate_one.sh
@@ -0,0 +1,51 @@
+export MASTER_PORT=1092
+
+bpe_dir=../../utils/BPE
+user_dir=../../ofa_module
+
+para_result=./$1
+para=${para_result}/checkpoint_best.pt
+
+#data=../../dataset/simmc2/simmc2_devtest_withlast.tsv
+data=../../../SPRING/dataset/simmc2/simmc2_devtest_withlast.tsv
+selected_cols=0,1,2,3
+split='test'
+
+generation_pred_json=${para_result}/test_predict.json
+#split_path=../../../simmcdata/simmc2_dials_dstc10_devtest.json
+split_path=../../../simmcdata/simmc2.1_dials_dstc11_devtest.json
+save_path=${para_result}/subtask-4-generation.json
+
+
+CUDA_VISIBLE_DEVICES=$2 python3 -m torch.distributed.launch --nproc_per_node=$3 --master_port=${MASTER_PORT} ../../evaluate.py \
+    ${data} \
+    --path=${para} \
+    --user-dir=${user_dir} \
+    --task=simmc2 \
+    --batch-size=12 \
+    --log-format=simple --log-interval=10 \
+    --seed=7 \
+    --gen-subset=${split} \
+    --results-path=${para_result} \
+    --beam=50 \
+    --max-len-b=100 \
+    --no-repeat-ngram-size=8 \
+    --fp16 \
+    --num-workers=0 \
+    --model-overrides="{\"data\":\"${data}\",\"bpe_dir\":\"${bpe_dir}\",\"eval_cider\":False,\"selected_cols\":\"${selected_cols}\"}"
+
+
+python3 format_task4_generation.py \
+    --generation-pred-json=${generation_pred_json} \
+    --split-path=${split_path} \
+    --save-path=${save_path}
+
+
+python3 response_evaluation_forall.py \
+    --data_json_path=${split_path} \
+    --model_response_path=${save_path} \
+    --para_name=${para} \
+    --bleu_path=${para_result} \
+    --single_round_evaluation
+
+
